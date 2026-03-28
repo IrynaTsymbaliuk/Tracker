a5e0b74482e8fe9161970c63486539ac164f3886
iryna.tsymbaliuk <iryna.tsymbaliuk.dev@gmail.com>
2026-03-28 22:20:54 +0900

    Release 4.0.0

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -11,12 +11,12 @@ android {
     defaultConfig {
         minSdk = 21
 
-        version = "3.0.0"
+        version = "4.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
 
-        buildConfigField("String", "LIBRARY_VERSION", "\"3.0.0\"")
+        buildConfigField("String", "LIBRARY_VERSION", "\"4.0.0\"")
         buildConfigField("String", "LIBRARY_NAME", "\"Tracker\"")
     }
 
@@ -68,7 +68,7 @@ afterEvaluate {
 
                 groupId = "com.github.IrynaTsymbaliuk"
                 artifactId = "tracker"
-                version = "3.0.0"
+                version = "4.0.0"
 
                 pom {
                     name.set("Tracker")
